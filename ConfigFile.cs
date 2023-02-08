2fedbcfd6ea0ee30e92e4867016fb687407453e1
Nyandisk <87699355+Nyandisk@users.noreply.github.com>
2023-02-08 22:00:52 +0200

    Add files via upload

---
+++ ConfigFile.cs
@@ -0,0 +1,116 @@
+﻿using System.Text;
+
+namespace VSFTConfig
+{
+    public class ConfigFile
+    {
+        public List<Section> Sections = new List<Section>();
+
+        private string _configPath;
+        private FileStream _configStream;
+
+        public ConfigFile(string inputPath) {
+            //check if the config path is valid
+            if (!File.Exists(inputPath))
+            {
+                throw new FileNotFoundException($"Config {inputPath} does not exist.");
+            }
+            //open and save stream
+            _configPath = inputPath;
+            _configStream = File.OpenRead(_configPath);
+            Sections = DetectSections();
+        }
+
+
+        //detect all sections and make them into a list
+        private List<Section> DetectSections()
+        {
+            //config lines
+            List<string> lines = GetConfigLines();
+
+            //section detection lists
+            List<(string, int)> sectionStarts = new List<(string, int)>();
+            List<Section> sections = new List<Section>();
+
+            int lineCounter = 0;
+            foreach (string line in lines)
+            {
+                //empty line
+                if (line.ToCharArray().Length == 0)
+                {
+                    lineCounter++;
+                    continue;
+                }
+                //line has section start token
+                if (line.ToCharArray()[0] == '>')
+                {
+                    //polish
+                    string unpolished = line.Split('>')[1];
+                    string name = unpolished.Replace(";","");
+
+                    //add to starts
+                    sectionStarts.Add((name,lineCounter));
+                }
+                lineCounter++;
+            }
+
+            //detect line endings
+            lineCounter = 0;
+            foreach ((string,int) start in sectionStarts)
+            {
+                try
+                {
+                    //find the first character of the next section
+                    int endl = sectionStarts[lineCounter + 1].Item2;
+
+                    sections.Add(new Section(start.Item1,start.Item2,endl,this));
+
+                }catch(ArgumentOutOfRangeException) //exception thrown when the section is the last section
+                {
+                    //get the last line
+                    int endl = lines.Count;
+
+                    sections.Add(new Section(start.Item1, start.Item2, endl, this));
+                }
+                lineCounter++;
+            }
+            return sections;
+        }
+
+        //return all file lines
+        public List<string> GetConfigLines()
+        {
+            //read bytes
+            byte[] byteContents = new byte[(int)_configStream.Length];
+            _configStream.Read(byteContents, 0, byteContents.Length);
+
+            //utf-8
+            string fullText = Encoding.UTF8.GetString(byteContents);
+
+            //return lines using ; as line-break
+            return new List<string>(fullText.Split(";"));
+        }
+
+        //clean-up
+        public void Dispose()
+        {
+            if (IsStreamDisposed())
+                return;
+            _configStream.Close();
+            _configStream.Dispose();
+        }
+
+        //check-up
+        private bool IsStreamDisposed()
+        {
+            try
+            {
+                _configStream.Flush();
+                return true;
+            }catch(ObjectDisposedException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Section.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSFTConfig
+{
+    public class Section
+    {
+        //public properties for common things
+        public string SectionName { get; set; }
+        public ConfigFile ParentConfig { get; set; }
+
+
+        //private line variables used for scanning the config
+        private int _endLine;
+        private int _startLine;
+
+        public Section(string name, int startLine, int endLine, ConfigFile config)
+        {
+            SectionName = name;
+            ParentConfig = config;
+            _startLine = startLine;
+            _endLine = endLine;
+        }
+        //get all key value pairs in the section
+        public List<Pair> GetSectionPairs()
+        {
+            List<Pair> pairs = new List<Pair>();
+            
+            int currentLine = 0;
+            foreach(string line in ParentConfig.GetConfigLines())
+            {
+                //if we are not past the starting line, we don't care. continue.
+                if (currentLine <= _startLine)
+                {
+                    currentLine++;
+                    continue;
+                }
+
+                //if we are at the end line, we have read everything. break.
+                if (currentLine == _endLine)
+                {
+                    currentLine++;
+                    break;
+                }
+
+                //if the line is empty, we don't care. continue.
+                if (line.ToCharArray().Length == 0 || string.IsNullOrWhiteSpace(line)) {
+                    currentLine++;
+                    continue;
+                }
+
+                //get pair
+                Pair pair = new(line.Split('=')[0], line.Split('=')[1]);
+                pairs.Add(pair);
+                currentLine++;
+            }
+            return pairs;
+        }
+
+        //get all the keys in a section
+        public List<string> GetKeys()
+        {
+            List<string> keys = new List<string>();
+            List<Pair> pairs = GetSectionPairs();
+
+            foreach (Pair p in pairs)
+            {
+                keys.Add(p.Key);
+            }
+            return keys;
+        }
+
+        //get value of a key (may be null)
+        public string? GetValue(string key)
+        {
+            List<Pair> pairs = GetSectionPairs();
+            foreach (Pair p in pairs)
+            {
+                if (p.Key.Equals(key))
+                    return p.Value;
+            }
+            return null;
+        }
+    }
+}
